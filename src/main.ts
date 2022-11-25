9806dea0bf3ead0a99c5560af73b055b04f819d3
bigbigking <2845758942@qq.com>
2022-11-25 10:29:06 +0800

    feat: pinia配置文件

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './style.css'
 import "tailwindcss/tailwind.css"
 import App from './App.vue'
 import router from '@/router'
+import { createPinia } from 'pinia'
 const app = createApp(App)
 app.use(router)
+app.use(createPinia())
 app.mount('#app')
