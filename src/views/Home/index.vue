d83c76db1a0df73c9c989f3a83f4dc9c8b00656b
bigbigking <2845758942@qq.com>
2022-12-12 15:56:18 +0800

    style: 设置zIndex

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,7 +6,7 @@
   <div class="pt-11">
     <!-- 搜索 -->
     <router-link 
-      class="w-screen h-11 fixed top-0 left-0 flex justify-center items-center bg-white shadow-md rounded-b-lg"
+      class="w-screen h-11 fixed top-0 left-0 flex justify-center items-center bg-white shadow-md rounded-b-lg z-10"
       to="/search"
       >
       <div class=" w-13/14 h-4/5 bg-light-500 rounded-full"></div>
