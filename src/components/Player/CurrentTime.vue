b5b669388f60f7219b6d001f8920f3c32cd9e51e
bigbigking <2845758942@qq.com>
2022-12-15 15:21:27 +0800

    feat: 时间戳格式化显示

--- src/components/Player/CurrentTime.vue
+++ src/components/Player/CurrentTime.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
 import Progress from '@/components/Progress/index.vue'
 import audioStore from '@/store/audioStore'
 import { storeToRefs } from 'pinia'
-const { currentTime, duration } = storeToRefs(audioStore())
+dayjs.extend(duration)
+const { currentTime, duration: durationTime } = storeToRefs(audioStore())
+// 时间戳格式化显示
+function forMatTime(time: number) {
+  const forMat = dayjs.duration(time).asHours() >= 1 ? 'HH:mm:ss' : 'mm:ss'
+  return dayjs.duration(time).format(forMat)
+}
+const currentTimeFormat = computed(() => {
+  return forMatTime(currentTime.value)
+})
+const durationTimeFormat = computed(() => {
+  return forMatTime(durationTime.value)
+})
+
 </script>
 
 <template>
   <div class="w-full">
-    <Progress :max="10" :min="0" v-model="currentTime" />
+    <Progress :max="durationTime" :min="0" v-model="currentTime" />
     <div class="w-full flex justify-between items-center text-dark-50 text-xs">
-      <span>{{ currentTime || '00:00' }}</span>
-      <span>{{ duration || '00:00'}}</span>
+      <span>{{ currentTimeFormat || '00:00' }}</span>
+      <span>{{ durationTimeFormat || '00:00'}}</span>
     </div>
   </div>
 </template>
