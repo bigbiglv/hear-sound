c5298752e93d40dd52fd7ce86cf8c58d8003543b
bigbigking <2845758942@qq.com>
2022-12-15 14:10:34 +0800

    feat: 进度数据绑定到store

--- src/components/Player/CurrentTime.vue
+++ src/components/Player/CurrentTime.vue
@@ -7,7 +7,7 @@ const { currentTime, duration } = storeToRefs(audioStore())
 
 <template>
   <div class="w-full">
-    <Progress :max="10" :min="0" />
+    <Progress :max="10" :min="0" v-model="currentTime" />
     <div class="w-full flex justify-between items-center text-dark-50 text-xs">
       <span>{{ currentTime || '00:00' }}</span>
       <span>{{ duration || '00:00'}}</span>
