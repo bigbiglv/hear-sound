821e70c350d6cd6b3ae33d6cb2f45509f4d0a92a
bigbigking <2845758942@qq.com>
2022-12-18 23:02:26 +0800

    feat: 进度条改变进度

--- src/components/Player/CurrentTime.vue
+++ src/components/Player/CurrentTime.vue
@@ -13,17 +13,30 @@ function forMatTime(time: number) {
   const forMat: string = hasHour ? 'HH:mm:ss' : 'mm:ss'
   return dayjs.duration(Number(time.toFixed(0)) * 1000).format(forMat)
 }
+// 当前播放的时间
 const currentTimeFormat = computed(() => {
   return forMatTime(currentTime.value)
 })
+// 音乐总时长
 const durationTimeFormat = computed(() => {
   return forMatTime(durationTime.value)
+})
+// 当前进度
+const progress = computed({
+  get(){
+    // 值来自audioStore的音乐进度
+    return durationTime.value
+  },
+  set(val){
+    // 改变的时候调用改变进度
+    audioStore().setCurrentTime(Number(val))
+  }
 })
 </script>
 
 <template>
   <div class="w-full">
-    <Progress :max="durationTime" :min="0" v-model="currentTime" />
+    <Progress :max="durationTime" :min="0" v-model="progress" />
     <div class="w-full flex justify-between items-center text-dark-50 text-xs">
       <span>{{ currentTimeFormat }}</span>
       <span>{{ durationTimeFormat }}</span>
