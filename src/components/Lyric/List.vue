e9536372bfe7c6b2243b6ad0514eb2c078d272f8
bigbigking <2845758942@qq.com>
2022-12-19 17:05:36 +0800

    feat: 歌词数组时间单位改为秒数与currentTime对应

--- src/components/Lyric/List.vue
+++ src/components/Lyric/List.vue
@@ -1,23 +1,57 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import audioStore from '@/store/audioStore'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+dayjs.extend(duration)
 const storeAudio = audioStore()
+
+// 时长转秒数 '00:01:00' => 60
+function formatSeconds(time: string): number {
+  const hasHour = time.split(":").length === 3
+  let hours: number
+  let minutes: number
+  let seconds: number
+  if(hasHour){
+    hours = Number(time.split(":")?.[0])
+    minutes = Number(time.split(":")?.[1])
+    seconds = parseInt(time.split(":")?.[2])
+  }else{
+    hours = 0
+    minutes = Number(time.split(":")?.[0])
+    seconds = parseInt(time.split(":")?.[1])
+  }
+  // 转化之后的秒数
+  const durationToseconds = dayjs.duration({
+    hours,
+    minutes,
+    seconds,
+  }).as('seconds')
+  return durationToseconds
+}
+// 歌词数组转格式 [ {time, words} ]
 const lyric = computed(() => {
   return storeAudio.lyric.lrc?.split('\n').map(item => {
-    let key = `${item.split(']')[0]}]`
-    let value = `${item.split(']')[1]}`
-    return [
-      key,value
-    ]
-  })
+    const key = `${item.split(']')[0]}`.substring(1)
+    const time = formatSeconds(key)
+    const words = `${item.split(']')[1]}`
+    return {
+      time, words
+    }
+    // 过滤空字段
+  }).filter(item => item['words'])
+})
+
+const onIndex = computed(() => {
+
 })
 </script>
 
 <template>
-  <div class="w-full h-64 overflow-y-auto flex justify-center ">
+  <div class="w-full h-3/4 overflow-y-auto flex justify-center ">
     <ul class="w-3/4">
-      <li v-for="lrc in lyric" class="text-center">
-        {{ lrc[1] }}
+      <li v-for="(lrc,index) in lyric" :key="lrc.time" class="text-center">
+        {{ lrc.words }}
       </li>
     </ul>
   </div>
