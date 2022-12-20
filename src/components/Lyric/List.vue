c32956dc3b29c66cadcc460959dd524aa7e48521
bigbigking <2845758942@qq.com>
2022-12-20 13:56:03 +0800

    feat: 歌词居中显示

--- src/components/Lyric/List.vue
+++ src/components/Lyric/List.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 import audioStore from '@/store/audioStore'
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import { storeToRefs } from 'pinia'
+
 dayjs.extend(duration)
 const storeAudio = audioStore()
 const { currentTime, lyric } = storeToRefs(storeAudio)
@@ -43,16 +44,55 @@ const lyricList = computed(() => {
   }).filter(item => item['words'])
 })
 
+// 当前播放的时间对应lyricList的time
 const onTime = computed(() => {
   const arr = lyricList.value?.filter(el => el.time <= currentTime.value)
-  console.log(arr)
-  return arr?.[arr.length - 1].time
+  return arr?.[arr.length - 1]?.time
 })
+
+/**
+ * 歌词变化时滚动
+ */
+// 最外层元素的Ref
+const contextRef = ref<HTMLElement | null>(null)
+// 里层ul元素
+const ulRef = ref<HTMLElement | null>(null)
+// 监听歌词位置变化
+watch(onTime, () => {
+  // 变化调整居中位置
+  scrollCenter()
+})
+
+/**
+ * 歌词滚动至居中位置
+ */
+function scrollCenter() {
+  // 每一句的歌词dom的集合
+  const liList = ulRef.value?.children
+  // 当句歌词的下标
+  const index = lyricList.value?.findIndex((lyr) => lyr.time === onTime.value) || 0
+  // 当句歌词的dom
+  const li = liList?.[index] as HTMLElement
+  const ulTop = ulRef.value?.offsetTop
+  // 当前歌词距离父元素 ul顶部的距离
+  const liTop = ulTop ? li?.offsetTop - ulTop : 0
+  const liHeight = li?.offsetHeight || 0
+  // 最外层容器高度
+  const contextHeight = contextRef.value?.offsetHeight || 0
+  // 滚动距离顶部的距离 居中
+  const top = liTop - contextHeight / 2 - liHeight / 2
+  contextRef.value?.scrollTo({
+    top,
+    behavior: 'smooth'
+  })
+}
+// 每次刚加载都刷新一次
+scrollCenter()
 </script>
 
 <template>
-  <div class="w-full h-3/4 overflow-y-auto flex justify-center">
-    <ul class="w-3/4">
+  <div class="w-full h-3/4 overflow-y-auto flex justify-center" ref="contextRef">
+    <ul class="w-3/4" ref="ulRef">
       <li 
         v-for="lrc in lyricList" 
         :key="lrc.time" 
