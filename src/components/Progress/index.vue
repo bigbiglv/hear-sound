78e4e2b16c7ae04848821abbfffb3efd56840b5b
bigbigking <2845758942@qq.com>
2022-12-21 15:18:51 +0800

    style: 已有距离的宽度样式

--- src/components/Progress/index.vue
+++ src/components/Progress/index.vue
@@ -34,9 +34,9 @@ const inputClass = computed(() => {
   }
 })
 
-const percentStyle = computed(() => {
-  let result = (value.value / props.max) * 100
-  return `width: ${result ? result.toFixed(0) : 0}%`
+// 已有距离 宽度style
+const usedStyle = computed(() => {
+  return `width: ${usedValue.value}px`
 })
 
 /**
@@ -50,14 +50,14 @@ const contextRef = ref<HTMLElement | null>(null)
 const { width: contextWidth } = useElementSize(contextRef)
 
 // 根据滑动的距离来限制范围
-const pointLeft = computed(() => {
+const usedValue = computed(() => {
   if(x.value < 0) return 0
   if(x.value > contextWidth.value) return contextWidth.value
   return x.value
 })
 // 交互圆点的left距离style
 const pointStyle = computed(() => {
-  return `left: ${pointLeft.value}px`
+  return `left: ${usedValue.value}px`
 })
 </script>
 
@@ -65,11 +65,14 @@ const pointStyle = computed(() => {
   <!-- <div class="transform origin-center flex justify-center items-center" :class="inputClass">
     <input type="range" v-model="value" :max="props.max" :min="props.min">
   </div> -->
-  <div class="relative w-36 mx-2 h-4 leading-3" ref="contextRef">
+  <div class="relative w-36 mx-10 h-4 leading-3" ref="contextRef">
     <!-- 总长度 --> 
-    <div class="absolute bg-light-100 w-full h-2 left-0 top-1/2 transform -translate-y-1/2 leading-3"></div>
+    <div
+      class="absolute bg-light-100 w-full h-2 left-0 top-1/2 transform -translate-y-1/2 leading-3"
+    >
+    </div>
     <!-- 已有长度 -->
-    <div class="absolute bg-red-500 h-2 left-0 top-1/2 transform -translate-y-1/2 leading-3" :style="percentStyle"></div>
+    <div class="absolute bg-red-500 h-2 left-0 top-1/2 transform -translate-y-1/2 leading-3" :style="usedStyle"></div>
     <!-- 交互圆点 -->
     <div 
       class="absolute w-4 h-4 rounded-full left-0 transform -translate-x-1/2 bg-white shadow-md"
